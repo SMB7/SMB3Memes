7fc9f2d735f5f419841c1845af1b08c94f3c02ab
SMB7 <calvinparmeter@gmail.com>
2019-03-15 15:17:06 -0600

    lolk testing still

---
+++ LoLKECL/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "LoLKECL",
+	"servers": [
+		"https://raw.githubusercontent.com/SMB7/SMB3Memes/master/LoLKECL/"
+	],
+	"title": "LoLK ECL Testing. Use at your own risk!"
+}
--- repo.js
+++ repo.js
@@ -3,7 +3,8 @@
 	"id": "SMB3Memes",
 	"patches": {
 		"EoSDChiptune": "EoSD Chiptune Soundtrack",
-		"LionKing": "Lion Sleeps Tonight Death Sound"
+		"LionKing": "Lion Sleeps Tonight Death Sound",
+		"LoLKECL": "LoLK ECL Testing. Use at your own risk!"
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/SMB7/SMB3Memes/master/"
