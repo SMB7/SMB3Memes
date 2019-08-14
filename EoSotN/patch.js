30b8e78abf4de49fd9e0f5c5640d018f05d50f4a
SMB7 <calvinparmeter@gmail.com>
2019-08-14 16:00:42 -0600

    added earliest version for testing

---
+++ EoSotN/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "EoSotN",
+	"servers": [
+		"https://raw.githubusercontent.com/SMB7/SMB3Memes/master/EoSotN/"
+	],
+	"title": "EoSD but I fucked up msg6.dat for a really dumb joke"
+}
--- repo.js
+++ repo.js
@@ -4,6 +4,7 @@
 	"patches": {
 		"EoSDChiptune": "EoSD Chiptune Soundtrack, lossless FLACs, DefleMask edition",
 		"EoSDChiptuneLossy": "EoSD Chiptune Soundtrack, lossy Oggs, DefleMask edition",
+		"EoSotN": "EoSD but I fucked up msg6.dat for a really dumb joke",
 		"LionKing": "Lion Sleeps Tonight Death Sound",
 		"LoLKECL": "LoLK ECL Testing. Use at your own risk!"
 	},
